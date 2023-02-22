02303e035a75a34b68ddd77ba417a938506faf11
Vladimir <ecrvmal@yandex.ru>
2023-02-22 00:03:03 -0800

    Lesson5 Initial commit

---
+++ TODO/management/commands/create_project_and_todo.py
@@ -0,0 +1,14 @@
+from mixer.backend.django import mixer
+from django.core.management.base import BaseCommand
+
+from TODO.models import Project, TODO
+
+
+class Command(BaseCommand):
+    help = 'Create 5 projects and 5 TODOs'
+
+    def handle(self, *args, **options):
+        for i in range(5):
+            mixer.blend(Project)
+            mixer.blend(TODO)
+        print('done')
